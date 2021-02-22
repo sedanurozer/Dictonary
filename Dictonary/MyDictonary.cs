af79d038cbb24100c315b65200cb910a71b54f56
asfalothsed <63976703+asfalothsed@users.noreply.github.com>
2021-02-22 22:34:04 +0300

    Add project files.

---
+++ Dictonary/MyDictonary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictonary<T>
+    {
+        T[] items;
+        T[] temp;
+        public MyDictonary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            temp = items;
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                items[i] = temp[i];
+            }
+            items[items.Length - 1] = item;
+        }
+        public void Listele()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine((i+1)+". Word:"+items[i]);
+            }
+        }
+       
+    }
+}
---
+++ Dictonary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<string> words = new MyDictonary<string>();
+            words.Add("Country");
+            words.Add("Apple");
+            words.Add("Travel");
+
+            words.Listele();
+        }
+    }
+}
